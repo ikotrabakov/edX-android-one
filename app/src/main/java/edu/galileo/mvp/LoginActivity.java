506776ec6bcfd3f0cc23636abe2d41fba7434f15
Ivaylo Kotrabakov <ikotrabakov@gmail.com>
2017-05-23 23:24:33 +0300

    Finished with LoginActivity

--- app/src/main/java/edu/galileo/mvp/LoginActivity.java
+++ app/src/main/java/edu/galileo/mvp/LoginActivity.java
@@ -16,12 +16,9 @@ import android.widget.Toast;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginView{
 
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
+    private LoginPresenter loginPresenter;
 
     // UI references.
     private AutoCompleteTextView mEmailView;
@@ -49,6 +46,9 @@ public class LoginActivity extends AppCompatActivity {
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        loginPresenter = new LoginPresenterImpl(this);
+
     }
 
     /**
@@ -57,10 +57,6 @@ public class LoginActivity extends AppCompatActivity {
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
-
         // Reset errors.
         mEmailView.setError(null);
         mPasswordView.setError(null);
@@ -69,17 +65,14 @@ public class LoginActivity extends AppCompatActivity {
         String email = mEmailView.getText().toString();
         String password = mPasswordView.getText().toString();
 
-        boolean cancel = false;
-        View focusView = null;
-
+        loginPresenter.validateCredentials(email, password);
 
     }
-
-
 
     /**
      * Shows the progress UI and hides the login form.
      */
+    @Override
     public void showProgress(final boolean show) {
         int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
@@ -102,6 +95,23 @@ public class LoginActivity extends AppCompatActivity {
                 mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
             }
         });
+    }
+
+    @Override
+    public void setUsernameError(int messageResId) {
+        mEmailView.setError(getString(messageResId));
+        mEmailView.requestFocus();
+    }
+
+    @Override
+    public void setPasswordError(int messageResId) {
+        mPasswordView.setError(getString(messageResId));
+        mPasswordView.requestFocus();
+    }
+
+    @Override
+    public void successAction() {
+        Toast.makeText(this, "Success!!", Toast.LENGTH_SHORT).show();
     }
 
 
